26ce171576f42fb28bee87c8372358f21db442e8
NaoyaYamaguchi0820 <naoya.yamaguchi.mail@gmail.com>
2020-08-22 17:43:51 +0900

    urlルーティングを設定

--- pythonWorkingGroupApplication/urls.py
+++ pythonWorkingGroupApplication/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # ルートURLへアクセスされた場合、mainアプリケーションのurls.pyへ処理を転送する
+    # (例)http://127.0.0.1:8000 にアクセスされた場合、ここにパターンマッチする
+    path('', include('main.urls')),
 ]
