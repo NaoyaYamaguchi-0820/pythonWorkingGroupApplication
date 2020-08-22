6883ff2391dc31746ce539de6d997a4cb2f24ddd
NaoyaYamaguchi0820 <naoya.yamaguchi.mail@gmail.com>
2020-08-22 17:16:06 +0900

    mainアプリケーション追加

--- pythonWorkingGroupApplication/settings.py
+++ pythonWorkingGroupApplication/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # mainアプリケーション追加
+    'main.apps.MainConfig',
 ]
 
 MIDDLEWARE = [
